d6e8a0193f7531aeaf197d45f4cd3fd232253827
SaiTeja <yatamsaiteja@gmail.com>
2020-08-07 21:55:14 -0400

    Payment part is completed

--- app/src/main/java/com/example/goshoes/MyCartPage.java
+++ app/src/main/java/com/example/goshoes/MyCartPage.java
@@ -28,12 +28,22 @@ public class MyCartPage extends AppCompatActivity {
         Intent intent = getIntent();
         final String received_Name =  intent.getStringExtra("shoe_name");
         final int received_Image = intent.getIntExtra("shoe_image",0);
-        String received_Price =  intent.getStringExtra("shoe_price");
+       final String received_Price =  intent.getStringExtra("shoe_price");
 
 
         btn1 = findViewById(R.id.minusbtn);
         btn2 = findViewById(R.id.plusbtn);
         checkoutbtn = findViewById(R.id.checkout);
+        checkoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyCartPage.this, PaymentOptions.class);
+                intent.putExtra("shoe_name",received_Name);
+                intent.putExtra("shoe_image",received_Image);
+                intent.putExtra("shoe_price",received_Price);
+                startActivity(intent);
+            }
+        });
 
         cart_imageview = findViewById(R.id.shoe_cart_imageview);
 
